68e93a90fbf0edfc3955eb2eb5dc4242a4341fb4
Sky273 <mail@luc-moreau.com>
2026-04-15 12:58:20 +0200

    oauth optionnel ne risque plus de casser l'auth

--- server/routes/auth/index.js
+++ server/routes/auth/index.js
@@ -6,15 +6,30 @@
 import express from 'express';
 import signinRoutes from './signin.routes.js';
 import usersRoutes from './users.routes.js';
-import googleRoutes from './google.routes.js';
-import passwordResetRoutes from './passwordReset.routes.js';
 
 const router = express.Router();
+
+function createLazyAuthRoute(modulePath) {
+    let routeModulePromise = null;
+
+    return async (req, res, next) => {
+        try {
+            if (!routeModulePromise) {
+                routeModulePromise = import(modulePath);
+            }
+
+            const routeModule = await routeModulePromise;
+            return routeModule.default(req, res, next);
+        } catch (error) {
+            return next(error);
+        }
+    };
+}
 
 // Mount sub-routes
 router.use('/', signinRoutes);
 router.use('/', usersRoutes);
-router.use('/', googleRoutes);
-router.use('/', passwordResetRoutes);
+router.use('/', createLazyAuthRoute('./google.routes.js'));
+router.use('/', createLazyAuthRoute('./passwordReset.routes.js'));
 
 export default router;
